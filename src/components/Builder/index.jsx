1cfc1512197973a831e7065a95b81ec5bc6a2ff4
jbarachadi <jbara.chadi@outlook.fr>
2025-01-03 11:33:04 +0100

    add: token generation and redirection and save to local storage and fetch from local storage

--- src/components/Builder/index.jsx
+++ src/components/Builder/index.jsx
@@ -24,7 +24,7 @@ const Builder = () => {
         {
           email: userData.user_email,
         },
-        { 
+        {
           headers: {
             'Content-Type': 'application/json',
           },
